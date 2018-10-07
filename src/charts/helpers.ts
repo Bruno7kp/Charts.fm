ee77336f510946f34cf135e871fa662c5aa2edd9
Bruno Camargo <bruno7kp@outlook.com>
2018-10-07 14:05:17 -0300

    start date fix

--- src/charts/helpers.ts
+++ src/charts/helpers.ts
@@ -1,12 +1,10 @@
-import moment from 'moment';
+import moment, { weekdays } from 'moment';
 import { Week, Artist, Album, Track, User, WeeklyCharts, MonthlyCharts, YearlyCharts } from '@/charts';
 
 const fixedStartDate = (start: Date, weekDay: number) => {
-    const n = moment(start).day(weekDay);
-    if (n.toDate() > start) {
-        n.subtract(1, 'weeks');
-    }
-    const date = n.toDate();
+    const n = moment(start);
+    const c = n.day(n.day() >= weekDay ? weekDay : (weekDay - 7));
+    const date = c.toDate();
     date.setHours(0);
     date.setMinutes(0);
     date.setSeconds(0);
